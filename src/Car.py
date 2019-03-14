3c0020b419a3f95b5e8935cbdb99fd2cf5b2b3b7
Navin <navinipe@gmail.com>
2019-03-14 14:22:21 +0530

    Wheels moved to function

--- src/Car.py
+++ src/Car.py
@@ -16,8 +16,8 @@ class Car:
     speed_range = range(10, 30, 5)#15
     chWd_range = range(5, 70, 5)#50#chassis width
     chHt_range = range(5, 50, 5)#10#chassis height
-    wheel1Radius_range = range(5, 25, 5)#15 
-    wheel2Radius_range = range(5, 25, 5)#15 
+    wheel1Radius_range = range(6, 25, 5)#15 
+    wheel2Radius_range = range(6, 25, 5)#15 
     chassisMass_range = range(10, 500, 50)#100
     wheel1Mass_range = range(10, 500, 50)#100
     wheel2Mass_range = range(10, 500, 50)#100  
@@ -33,6 +33,10 @@ class Car:
     #---parts to add or remove from space
     chassis_b = []
     chassis_s = []
+    wheel1_b = []
+    wheel1_s = []
+    wheel2_b = []
+    wheel2_s = []    
     
     def __init__(self, space, physics, xOffset, yOffset):
         self.space = space
@@ -68,20 +72,37 @@ class Car:
         moment = self.physics.moment_for_box(self.chassisMass, (self.chWd, self.chHt))
         self.chassis_b = self.physics.Body(self.chassisMass, moment)
         self.chassis_s = self.physics.Poly.create_box(self.chassis_b, (self.chWd, self.chHt))
+        self.chassis_s.color = 10,150,40
         self.chassis_b.position = chassisXY + (0, 0)
         self.space.add(self.chassis_b, self.chassis_s)           
         
-        
-        #w1 = self.addChassis(chassisXY, -chWd, 0, mass, 5, 30) #special rectangular wheel
-        w1 = self.addWheel(chassisXY, self.chWd, 0, self.wheel1Mass, self.wheel1Radius)
-        w2 = self.addWheel(chassisXY, -self.chWd, 0, self.wheel2Mass, self.wheel2Radius)
+        #---wheel1 (left side wheel)
+        #w1 = self.addChassis(chassisXY, -chWd, 0, mass, 5, 30) #special rectangular wheel        
+        moment = self.physics.moment_for_circle(self.wheel1Mass, 0, self.wheel1Radius)
+        self.wheel1_b = self.physics.Body(self.wheel1Mass, moment)
+        self.wheel1_s = self.physics.Circle(self.wheel1_b, self.wheel1Radius)
+        self.wheel1_s.friction = self.friction
+        self.wheel1_s.color = 180,180,180
+        self.wheel1_b.position = chassisXY - (self.chWd, 0)
+        self.space.add(self.wheel1_b, self.wheel1_s)          
 
-        self.space.add(self.physics.PinJoint(w1, self.chassis_b, (0,0), (-self.chWd/2,0)), 
-                       self.physics.PinJoint(w2, self.chassis_b, (0,0), (self.chWd/2,0)),
-                       self.physics.PinJoint(w1, self.chassis_b, (0,0), (0,-self.chHt/2)), 
-                       self.physics.PinJoint(w2, self.chassis_b, (0,0), (0,-self.chHt/2)))  
-        motorJoint1 = self.physics.SimpleMotor(w1, self.chassis_b, self.speed); motorJoint1.max_force = 10000000
-        motorJoint2 = self.physics.SimpleMotor(w2, self.chassis_b, self.speed); motorJoint2.max_force = 10000000
+        #---wheel2 (right side wheel)
+        #w1 = self.addChassis(chassisXY, -chWd, 0, mass, 5, 30) #special rectangular wheel        
+        moment = self.physics.moment_for_circle(self.wheel2Mass, 0, self.wheel2Radius)
+        self.wheel2_b = self.physics.Body(self.wheel2Mass, moment)
+        self.wheel2_s = self.physics.Circle(self.wheel2_b, self.wheel2Radius)
+        self.wheel2_s.friction = self.friction
+        self.wheel2_s.color = 180,180,180
+        self.wheel2_b.position = chassisXY - (-self.chWd, 0)
+        self.space.add(self.wheel2_b, self.wheel2_s)         
+        
+
+        self.space.add(self.physics.PinJoint(self.wheel1_b, self.chassis_b, (0,0), (-self.chWd/2,0)), 
+                       self.physics.PinJoint(self.wheel2_b, self.chassis_b, (0,0), (self.chWd/2,0)),
+                       self.physics.PinJoint(self.wheel1_b, self.chassis_b, (0,0), (0,-self.chHt/2)), 
+                       self.physics.PinJoint(self.wheel2_b, self.chassis_b, (0,0), (0,-self.chHt/2)))  
+        motorJoint1 = self.physics.SimpleMotor(self.wheel1_b, self.chassis_b, self.speed); motorJoint1.max_force = 10000000
+        motorJoint2 = self.physics.SimpleMotor(self.wheel2_b, self.chassis_b, self.speed); motorJoint2.max_force = 10000000
         self.space.add(motorJoint1, motorJoint2)
         
     def removeCar(self):
